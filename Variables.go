4ea562a6dd599a72f05d23d484e45fabd51fad4f
Vikram.R <vikram.r@sysarcinfomatix.com>
2019-01-31 11:16:05 +0530

    Static Type Declaration and Dynamic Type Declaration

---
+++ Variables.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	/* Static Type Declaration in Go */
+	var x float64 = 20.0
+	fmt.Println(x)
+	/* Dynamic Type Declaration / Type Inference in Go */
+	y := 42
+	fmt.Println(y)
+	name := "vikram"
+	fmt.Println(name)
+}
